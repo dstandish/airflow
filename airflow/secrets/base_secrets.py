70aa7ab8ec52397b0e81422a193f6de9e58f027a
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2021-11-26 20:50:29 -0800

    --wip-- [skip ci]

--- airflow/secrets/base_secrets.py
+++ airflow/secrets/base_secrets.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
+import json
 import warnings
 from abc import ABC
 from typing import TYPE_CHECKING, List, Optional
@@ -26,8 +26,10 @@ if TYPE_CHECKING:
 class BaseSecretsBackend(ABC):
     """Abstract base class to retrieve Connection object given a conn_id or Variable given a key"""
 
-    def __init__(self, **kwargs):
-        pass
+    __NO_DEFAULT_SENTINEL = object()
+
+    def __init__(self, connections_as_json: bool = False, **kwargs):
+        self.connections_as_json = connections_as_json
 
     @staticmethod
     def build_path(path_prefix: str, secret_id: str, sep: str = "/") -> str:
@@ -47,25 +49,56 @@ class BaseSecretsBackend(ABC):
         """
         Get conn_uri from Secrets Backend
 
+        This method is deprecated and will be removed in a future release; implement ``get_conn_value`` instead.
+
         :param conn_id: connection id
         :type conn_id: str
         """
         raise NotImplementedError()
 
+    def get_conn_value(self, conn_id: str) -> Optional[str]:
+        raise NotImplementedError
+
+    def _build_conn_from_value(self, conn_id, value):
+        from airflow.models.connection import Connection
+
+        if self.connections_as_json:
+            kwargs = json.loads(value)
+            extra = kwargs.pop('extra', None)
+            if extra:
+                kwargs['extra'] = extra if isinstance(extra, str) else json.dumps(extra)
+            return Connection(conn_id=conn_id, **kwargs)
+        else:
+            return Connection(conn_id=conn_id, uri=value)
+
     def get_connection(self, conn_id: str) -> Optional['Connection']:
         """
         Return connection object with a given ``conn_id``.
 
+        Tries ``get_conn_value`` first and if not implement, tries ``get_conn_
+
         :param conn_id: connection id
         :type conn_id: str
         """
-        from airflow.models.connection import Connection
 
-        conn_uri = self.get_conn_uri(conn_id=conn_id)
-        if not conn_uri:
+        conn_value = None
+
+        # TODO: after removal of ``get_conn_uri`` we should not catch NotImplementedError here
+        try:
+            conn_value = self.get_conn_value(conn_id=conn_id)
+        except NotImplementedError:
+            try:
+                conn_value = self.get_conn_uri(conn_id=conn_id)
+            except NotImplementedError:  # we only catch here to allow use of only ``get_conn_value``
+                pass
+
+        # if get_conn_value was implemented, we should use it;
+        # if conn_value is self.__NO_DEFAULT_SENTINEL:
+
+        if conn_value and conn_value is not self.__NO_DEFAULT_SENTINEL:
+            return self._build_conn_from_value(conn_id=conn_id, value=conn_value)
+        else:
             return None
-        conn = Connection(conn_id=conn_id, uri=conn_uri)
-        return conn
 
     def get_connections(self, conn_id: str) -> List['Connection']:
         """
